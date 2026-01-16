52dc7057df7fcac39ee3c564b444357a527675cf
Jesus <sosarolliedev@gmail.com>
2026-01-15 21:34:54 -0300

    add more basic functions before adding more classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using contoso_receipt_backend;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,6 +15,8 @@ var receipts = app.MapGroup("/receipts");
 receipts.MapGet("/", GetAllReceipts);
 receipts.MapPost("/", CreateReceipt);
 receipts.MapGet("/{id}", GetReceiptById);
+receipts.MapPut("/{id}", UpdateReceipt);
+receipts.MapDelete("/{id}", DeleteReceiptById);
 
 app.Run();
 
@@ -34,10 +37,39 @@ static async Task<IResult> CreateReceipt(Receipt rec, ReceiptDB db)
     return TypedResults.Created($"/receipts/{rec.Id}", dto);  
 }
 
+static async Task<IResult> UpdateReceipt(int id, ReceiptDTO recDTO, ReceiptDB db)
+{
+    var receipt = await db.Receipts.FindAsync(id);
+    if (receipt is null) return TypedResults.NotFound();
+    else
+    {
+        //map the updated values from the DTO to the entity
+        receipt.Receipt_date = recDTO.Receipt_date;
+        receipt.Total_amount = recDTO.Total_amount;
+        receipt.Proper_name = recDTO.Proper_name;
+        receipt.Email = recDTO.Email;
+        receipt.Name = recDTO.Name;
+
+        await db.SaveChangesAsync();
+        return TypedResults.NoContent();
+    }
+}
+
 static async Task<IResult> GetReceiptById(int id, ReceiptDB db)
 {
     return await db.Receipts.FindAsync(id) //EF method to find by its ID (PK)
         is Receipt rec // if that is a receipt
             ? TypedResults.Ok(new ReceiptDTO(rec)) //return the DTO
             : TypedResults.NotFound(); //else return not found
+}
+
+static async Task<IResult> DeleteReceiptById(int id, ReceiptDB db)
+{
+    if (await db.Receipts.FindAsync(id) is Receipt rec)
+    {
+        db.Receipts.Remove(rec);
+        await db.SaveChangesAsync();
+        return TypedResults.NoContent();
+    }
+    return TypedResults.NotFound();
 }
